4e4e1d2a43c4530ea4baa554eae858d897e9ce12
Hadassah Stulman <dasstlman@gmail.com>
2022-10-25 17:06:22 +0300

    initial code + Welcome functions

---
+++ dotNet5783_2430_2132/Stage0/Program2430.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Stage0
+{
+    partial class Program
+    {
+        static void main()
+        {
+            Welcome2430();
+            Welcome2132();
+            Console.ReadKey();
+        }
+
+        private static void Welcome2430()
+        {
+            Console.Write("Enter your name: ");
+            string name;
+            name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first {1} colsole aplication", name);
+        }
+
+        static partial void Welcome2132();
+    }
+}
