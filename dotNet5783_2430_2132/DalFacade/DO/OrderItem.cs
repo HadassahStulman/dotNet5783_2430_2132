ae69a6a1704c309605fbe4ed9d04cae5a44a4a12
Esther Malka <enusbach@g.jct.ac.il>
2022-11-03 11:59:56 +0200

    sync

--- dotNet5783_2430_2132/DalFacade/DO/OrderItem.cs
+++ dotNet5783_2430_2132/DalFacade/DO/OrderItem.cs
@@ -1,7 +1,36 @@
 ﻿
 
+using static DO.Enums;
+using System.Xml.Linq;
+
 namespace DO;
 
 public struct OrderItem
 {
+    /// <summary>
+    /// Uniqe ID for identifing a item (in this order)
+    /// </summary>
+    public int ProductId { get; set; }
+    /// <summary>
+    /// Uniqe ID for identifing the order (that contains this item)
+    /// </summary>
+    public int OrderId { get; set; }
+    /// <summary>
+    /// price for item
+    /// </summary>
+    public double Price { get; set; }
+    /// <summary>
+    /// amount of this item in order
+    /// </summary>
+    public int Amount { get; set; }
+    /// <summary>
+    /// prints decription of a item in order
+    /// </summary>
+    /// <returns>string</returns>
+    public override string ToString() => $@"
+        Product ID: {ProductId}
+        Order ID: {OrderId}
+    	Price: {Price}
+    	Amount of product: {Amount}
+";
 }
