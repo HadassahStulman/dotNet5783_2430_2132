5e50773e738db3031c35042c12f0b7305237805a
Hadassah Stulman <dasstlman@gmail.com>
2022-11-03 11:03:40 +0200

    building projects classes

---
+++ dotNet5783_2430_2132/DalFacade/DO/Enums.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace DO;
+
+public struct Enums
+{
+   public enum Category {TextBooks, // school and study books
+                    CookBooks, // recipes
+                    ToddlerBooks, // children and first reading books
+                    ReligiousBooks,  // jewish textbooks
+                    ReadingBooks}; // different genre of books for pleasure (novels, fantacy...)
+}
---
+++ dotNet5783_2430_2132/DalFacade/DO/Products.cs
@@ -0,0 +1,42 @@
+﻿
+using static DO.Enums;
+
+namespace DO;
+
+/// <summary>
+/// Structure for a Book
+/// </summary>
+public struct Products
+{
+    /// <summary>
+    /// Uniqe ID for identifing a book
+    /// </summary>
+    public int ID { get; set; }
+    /// <summary>
+    /// name of the book
+    /// </summary>
+    public string Name { get; set; }
+    /// <summary>
+    /// price for book
+    /// </summary>
+    public double Price { get; set; }
+    /// <summary>
+    /// the category of the book
+    /// </summary>
+    public Category Category { get; set; }
+    /// <summary>
+    /// amount of copies in stock 
+    /// </summary>
+    public int InStock { get; set; }
+
+    /// <summary>
+    /// prints decription of object (book)
+    /// </summary>
+    /// <returns>string</returns>
+    public override string ToString() => $@"
+    Product ID={ID}: {Name}
+    cateory - {Category}
+    Price: {Price}
+    Amount in stock: {InStock}";
+  
+}
