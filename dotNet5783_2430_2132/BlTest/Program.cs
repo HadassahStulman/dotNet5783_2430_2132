a01e116f4305c3dc6dce62dad5839408a795ff13
Hadassah Stulman <dasstlman@gmail.com>
2022-11-25 15:34:25 +0200

    comparing and minor changes

--- dotNet5783_2430_2132/BlTest/Program.cs
+++ dotNet5783_2430_2132/BlTest/Program.cs
@@ -4,7 +4,11 @@ namespace BL;
 
 public class Program
 {
-    private BlApi.IBl Bl = new BlApi();
+
+    /// <summary>
+    /// in order to allow acsses ta all bl methods
+    /// </summary>
+    private BlApi.IBl Bl = new BlImplementation.Bl();
     static void Main()
     {
         try
@@ -40,6 +44,29 @@ public class Program
         catch (Exception ex)
         {
             Console.WriteLine(ex.Message);
+        }
+    }
+    private static void manageProduct()
+    {
+        Console.WriteLine(@"enter: 1 for adding a new book
+       2 for getting a book description for MANAGAER according to ID
+       3 for getting a book description for CUSTOMER according to ID
+       4 for getting catalog of all books
+       5 for updating an existing book 
+       6 for deleting a book
+       0 for returning back to the main menu ");
+        int ch1;
+        int.TryParse(Console.ReadLine(), out ch1); // converts the input to integer
+        switch (ch1)
+        {
+            case 1: AddNewBook(); break;
+            case 2: PrintDescriptionMANGER(); break;
+            case 3: PrintDescriptionCUSTOMER(); break;
+            case 4: GetAllBooks(); break;
+            case 5: UpdateBook(); break;
+            case 6: DeleteBook(); break;
+            default:
+                break;
         }
     }
 }
