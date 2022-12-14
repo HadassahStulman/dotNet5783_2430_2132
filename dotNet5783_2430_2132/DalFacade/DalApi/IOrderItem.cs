947ed75754e2451b2ff187d2cf089e8e8c85a316
Hadassah Stulman <dasstlman@gmail.com>
2022-12-14 19:00:03 +0200

    fixes for datge 4

--- dotNet5783_2430_2132/DalFacade/DalApi/IOrderItem.cs
+++ dotNet5783_2430_2132/DalFacade/DalApi/IOrderItem.cs
@@ -1,17 +1,11 @@
 ﻿using DO;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DalApi
+namespace DalApi;
+
+/// <summary>
+/// interface for Order item behavior
+/// </summary>
+public interface IOrderItem:ICrud<OrderItem>
 {
-    /// <summary>
-    /// interface for Order item behavior
-    /// </summary>
-    public interface IOrderItem:ICrud<OrderItem>
-    {
- 
-    }
+
 }
