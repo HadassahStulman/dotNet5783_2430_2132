e59d29159e0bd0b42bf77c46b538a62dddc5b1cd
Hadassah Stulman <dasstlman@gmail.com>
2023-01-15 00:40:28 +0200

    bonus

--- dotNet5783_2430_2132/PL/TrackOrderDisplayWindow.xaml.cs
+++ dotNet5783_2430_2132/PL/TrackOrderDisplayWindow.xaml.cs
@@ -46,17 +46,17 @@ public partial class TrackOrderDiplayWindow : Window
         }
  
     }
-    /// <summary>
-    /// the event changes the buttons do be enabled 
-    /// </summary>
-    /// <param name="sender"></param>
-    /// <param name="e"></param>
-    private void ID_TextBox_TextChanged(object sender, TextChangedEventArgs e)
-    {
-        TrackOrder_Butten.IsEnabled = true;
-        ViewOrder_Button.IsEnabled = true;
+    ///// <summary>
+    ///// the event changes the buttons do be enabled 
+    ///// </summary>
+    ///// <param name="sender"></param>
+    ///// <param name="e"></param>
+    //private void ID_TextBox_TextChanged(object sender, TextChangedEventArgs e)
+    //{
+    //    TrackOrder_Butten.IsEnabled = true;
+    //    ViewOrder_Button.IsEnabled = true;
 
-    }
+    //}
 
     /// <summary>
     /// opening a window with details of a order
